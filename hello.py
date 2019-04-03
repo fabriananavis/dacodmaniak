5da33fbe70783fb363b214b614a55af206d58eee
fabriana <fabriana.navis@gmail.com>
2019-04-03 12:28:07 +0200

    Ajout des opérandes

---
+++ hello.py
@@ -0,0 +1,20 @@
+"""
+Algorithm #0.0.1
+Declare var
+Store some arithmetics into the Var
+Display result
+Operands and Operators, functions
+
+@version 0.0.2
+Add two operands and replace compute method
+"""
+
+resutats = 0 # Definition de la variable
+operands1 = -3
+operande2 = 2
+resultat = 2 + 2
+print(resultat)
+
+"""
+Fin de l'algorithme
+"""
